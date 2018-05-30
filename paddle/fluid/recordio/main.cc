47670eb8a7a778da9c0fe47b5c12875c37e7a4aa
yuyang18 <reyoung@126.com>
2018-05-30 18:58:27 +0800

    Bad Data

--- paddle/fluid/recordio/main.cc
+++ paddle/fluid/recordio/main.cc
@@ -49,13 +49,13 @@ int main() {
     input_vars.emplace_back();
     new_scope.Var(&input_vars.back())
         ->GetMutable<LoDTensor>()
-        ->Resize({40, 128, 14, 14})
+        ->Resize({64, 3, 224, 224})
         .mutable_data<float>(CUDAPlace(i));
 
     filter_vars.emplace_back();
     new_scope.Var(&filter_vars.back())
         ->GetMutable<LoDTensor>()
-        ->Resize({128, 128, 3, 3})
+        ->Resize({128, 3, 3, 3})
         .mutable_data<float>(CUDAPlace(i));
 
     out_vars.emplace_back();
