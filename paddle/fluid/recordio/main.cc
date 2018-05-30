476af96aba6d70a3cee289394eed3d8af4169e42
yuyang18 <reyoung@126.com>
2018-05-30 18:54:53 +0800

    Up

--- paddle/fluid/recordio/main.cc
+++ paddle/fluid/recordio/main.cc
@@ -49,13 +49,13 @@ int main() {
     input_vars.emplace_back();
     new_scope.Var(&input_vars.back())
         ->GetMutable<LoDTensor>()
-        ->Resize({40, 256, 56, 56})
+        ->Resize({40, 1024, 14, 14})
         .mutable_data<float>(CUDAPlace(i));
 
     filter_vars.emplace_back();
     new_scope.Var(&filter_vars.back())
         ->GetMutable<LoDTensor>()
-        ->Resize({256, 256, 1, 1})
+        ->Resize({1024, 1024, 1, 1})
         .mutable_data<float>(CUDAPlace(i));
 
     out_vars.emplace_back();
